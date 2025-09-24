5b667fb6e93405d2cce5b1162b887ff2c2289be3
Calvin Huynh <calvinhuynh2001@gmail.com>
2025-09-24 02:50:48 -0400

    # Mobile responsiveness updates to home page - Updated responsiveness for home page - Updated and added some logos

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -17,40 +17,51 @@ defineProps({
 
     <!-- Main Container -->
     <div
-        class="flex flex-col min-h-screen bg-background items-center justify-center"
+        class="flex flex-col md:flex-row lg:flex-col min-h-screen bg-background items-center justify-center p-4 gap-4"
     >
         <!-- Hero Section -->
         <section
-            class="bg-primary rounded-lg max-w-7xl flex flex-col lg:flex-row items-center justify-between p-8 gap-8 shadow-lg"
+            class="flex flex-col max-w-7xl bg-background lg:bg-primary rounded-lg lg:flex-row lg:m-8 lg:p-8 lg:gap-8 lg:shadow-lg items-center justify-center lg:justify-between"
         >
             <div class="flex-1">
-                <h1 class="text-white text-4xl md:text-6xl lg:text-7xl">
+                <h1
+                    class="hidden lg:block text-white text-4xl lg:text-6xl xl:text-7xl"
+                >
                     Welcome to Bridge!
                 </h1>
-                <h2 class="text-hover text-lg md:text-2xl lg:text-4xl">
+                <h2
+                    class="hidden lg:block text-hover text-lg lg:text-2xl xl:text-4xl"
+                >
                     Where stories are shared, and friendships begin
                 </h2>
             </div>
-            <div class="flex-shrink-0 w-1/5">
+            <div class="w-3/4 md:w-3/5 lg:w-1/5">
+                <img
+                    src="/images/logos/logo-with-name-purple.svg"
+                    alt="Bridge Logo"
+                    class="w-full h-full object-contain lg:hidden"
+                />
                 <img
                     src="/images/logos/logo-white.svg"
                     alt="Bridge Logo"
-                    class="w-full aspect-square object-contain"
+                    class="w-full aspect-square object-contain hidden lg:block"
                 />
             </div>
         </section>
 
-        <section class="flex flex-col justify-center mt-10">
+        <section
+            class="flex flex-col mx-8 md:w-full justify-center lg:items-center"
+        >
             <div class="max-w-7xl">
                 <h2 class="text-hover text-center text-4xl font-bold">
                     Get Started
                 </h2>
             </div>
             <!-- Three Buttons -->
-            <div class="flex flex-col sm:flex-row gap-4">
-                <CustomButton text="Resident" preset="neutral" size="large" />
-                <CustomButton text="Volunteer" preset="neutral" size="large" />
-                <CustomButton text="Admin" preset="neutral" size="large" />
+            <div class="flex flex-col lg:flex-row gap-4">
+                <CustomButton text="Resident" preset="neutral" size="medium" />
+                <CustomButton text="Volunteer" preset="neutral" size="medium" />
+                <CustomButton text="Admin" preset="neutral" size="medium" />
             </div>
         </section>
     </div>
